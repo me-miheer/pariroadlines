6c4ea81ddd638939e80e550b05b9e1079c288b92
me-miheer <me.miheer@gmail.com>
2023-10-03 16:20:13 +0530

    added

--- resources/views/billing/invoice-editor.blade.php
+++ resources/views/billing/invoice-editor.blade.php
@@ -341,7 +341,7 @@
             str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
             str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
             str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-            str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + ' rupees only ' : '';
+            str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
             return str;
         }
 
@@ -351,7 +351,7 @@
                 if(wordData =='overflow'){
                     $('#amount_in_words').val('')
                 }else{
-                    $('#amount_in_words').val(wordData)
+                    $('#amount_in_words').val(wordData+" rupees only")
                 }
             }
         }
